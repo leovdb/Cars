172fa155970994fbf64b97aee889d099fa088ae0
leovdb <leo@driven-cars.com>
2015-02-05 16:38:31 +0000

    Moved email functionality to target page
    Moved email functionality to target page to simplify form completion
    from different pages. Also added referral capability for home page.

--- thankyou.php
+++ thankyou.php
@@ -1,3 +1,103 @@
+<?php
+
+   // define variables and set to null
+   $name = NULL;
+   $email = NULL;
+   $mileage = NULL;
+   $postcode = NULL;
+   $reg = NULL;
+   $condition = NULL;
+   $phone = NULL;
+   $ref = "";
+   $source = "";
+   
+   $email_error = NULL;
+
+   // quick function to strip hacky crap
+   function clean_input($data) 
+   {
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
+   }
+
+   // if post then process and route otherwise show form
+   if( $_SERVER["REQUEST_METHOD"] == "POST" ) 
+   {
+      $name = clean_input($_POST["name"]);
+      $email = clean_input($_POST["email"]);
+	  $postcode = clean_input($_POST["postcode"]);
+	  $reg = clean_input($_POST["reg"]);
+	  $mileage = clean_input($_POST["mileage"]);
+	  $condition = clean_input($_POST["condition"]);
+	  $phone = clean_input($_POST["phone"]);
+	  
+	  if(isset($_POST["ref"]))
+	  {
+		$ref = clean_input($_POST["ref"]);
+	  }
+	  if(isset($_POST["source"]))
+	  {
+		$source = clean_input($_POST["source"]);
+	  }
+
+      // various rules go here
+      // this one reflects that name is required
+      if( empty($_POST["name"]) )
+      {
+         $name_error = "Name is required";
+      }
+      else 
+      {
+         $name = clean_input($_POST["name"]);
+      }
+
+      // this one checks that email is valid 
+      if( ! filter_var($email, FILTER_VALIDATE_EMAIL) ) 
+      {
+         $email_error = "Invalid email format"; 
+      }
+
+      // can set any rules here
+      // verify email1 matches email2 for example
+
+      // Then if no errors proceed to email and route 
+      // else nothing and proceed to represent form below
+      if( $name_error === NULL & $email_error === NULL )
+      {
+         $to       = "leo@driven-cars.com";
+         $subject  = "Website registration";
+         
+		 $message .= "Name: ".$name."\r\n";
+         $message .= "Email: ".$email."\r\n";
+		 $message .= "Reg #: ".$reg."\r\n";
+		 $message .= "Mileage: ".$mileage."\r\n";
+		 $message .= "Condition: ".$condition."\r\n";
+		 $message .= "Postcode: ".$postcode."\r\n";
+		 $message .= "Phone: ".$phone."\r\n";
+		 $message .= "Referrer: ".$ref."\r\n";
+		 $message .= "Source: ".$source."\r\n";
+		 
+         $headers  = "From: leo@driven-cars.com"."\r\n"."Reply-To: leo@driven-cars.com";
+
+         // For simplicity use PHP's inbuilt mail function, 
+         // works with most hosting out the box.
+         if(!mail($to, $subject, $message, $headers))
+		 {
+		
+         // Then simply route to an error url - header will return an error 
+         // if the page outputs anything to the page before it hence all this
+         // PHP happens before the HTML code below.
+         	 header('Location: http://www.driven-cars.com/error.php');
+		 }
+		 		 
+      }
+
+   }
+   
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
